16cdd58f413054a8439315122e743392a569d980
Abiy Seifu <abiy@ripple.com>
2014-04-08 00:59:52 -0700

    [CHORE] adjust low balance and corrected typo on email addresses

--- config/inbound.js
+++ config/inbound.js
@@ -15,12 +15,12 @@ nconf.defaults({
 	'XRP_FUND' : 'r123',
 	'XRP_FUND_SECRET' : 'sXYZ',
 	'DISCOUNT_PERCENTAGE' : 10,
-	'BTC_RECEIVE_NOTIFY' : ['abiy@rippple.com'],
-	'XRP_SENT_NOTIFY' : ['abiy@rippple.com'],
+	'BTC_RECEIVE_NOTIFY' : ['abiy@ripple.com'],
+	'XRP_SENT_NOTIFY' : ['abiy@ripple.com'],
 	'SANE_PRICE_MIN' : 20000,
 	'SANE_PRICE_MAX' : 40000,
-	'XRP_LOW_BALANCE_WARNING' : 100000,
-	'XRP_LOW_BALANCE_EMAIL' : ['abiy@rippple.com'],
+	'XRP_LOW_BALANCE_WARNING' : 40,
+	'XRP_LOW_BALANCE_EMAIL' : ['abiy@ripple.com'],
 	'NUMBER_OF_CONFIRMATIONS': 2,
     'TOTAL_BTC_RECEIVED': 0
 });
