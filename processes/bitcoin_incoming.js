96dd0cb85029683b044a27d0d491a15026836a04
Abiy Seifu <abiy@ripple.com>
2014-04-08 14:37:12 -0700

    [CHORE] update config file

--- processes/bitcoin_incoming.js
+++ processes/bitcoin_incoming.js
@@ -1,5 +1,5 @@
 var InboundBtc = require('../lib/blockchain_client.js'),
-	conf = require('../config/inbound.js'),
+	config = require('../config/nconf'),
     pollHotWalletBalance = require('../lib/poll_hot_wallet_balance.js');
 
 var inboundBtc = new InboundBtc;
@@ -7,11 +7,11 @@ var inboundBtc = new InboundBtc;
 var Poll=  {
     inbound: function(){
         var self = this;
-        inboundBtc.poll(conf.get('BTC_INBOUND'));
-        //TODO: listen to success before set time out
+        inboundBtc.poll(config.get('BTC_INBOUND'));
         setTimeout(function(){
             self.inbound();
         }, 10000);
+
     },
     hotWalletBalance: pollHotWalletBalance
 };
